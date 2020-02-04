551075480b6facff975f1a3b31d79a2aa7d5b63b
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-02-04 13:58:48 -0500

    Updated placeholder fax number

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
       </div>
       <div class="col text-center text-lg-right">
         <p class="address mb-0 text-secondary">Address / <a href="tel:518-880-6860" class="text-secondary">Phone:
-            123-456-7899</a> / Fax: 518-880-6865</p>
+            123-456-7899</a> / Fax: 518-123-4567</p>
       </div>
     </div>
 
