0d10795f6c5a8aefbacd00d9f09c083871b7b2d7
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-03-05 09:05:41 -0500

    Updated footer text

--- includes/footer.php
+++ includes/footer.php
@@ -2,13 +2,10 @@
   <div class="container">
     <div class="row d-flex flex-column flex-lg-row">
       <div class="col text-center text-lg-left">
-        <p class="address mb-0 text-secondary">&copy; <?php echo date("Y"); ?> IBI Semper Training (IST)</p>
+        <p class="address mb-0 text-secondary">&copy; <?php echo date("Y"); ?> Ibi Semper Training (IST)</p>
       </div>
       <div class="col text-center text-lg-right">
-        <p class="address mb-0 text-secondary"><a href="tel:518-605-3976" class="text-secondary mr-2"><i
-              class="fas fa-phone-alt"></i> Phone: 518-605-3976</a> / <a
-            href="mailto:ibisempertraining@gmail.com?subject=Ibi Semper Website Inquiry" class="text-secondary ml-2"><i
-              class="far fa-envelope"></i> Email</a></p>
+        <p class="address mb-0 text-secondary"><a href="tel:518-605-3976" class="text-secondary mr-2"><i class="fas fa-phone-alt"></i> Phone: 518-605-3976</a> / <a href="mailto:ibisempertraining@gmail.com?subject=Ibi Semper Website Inquiry" class="text-secondary ml-2"><i class="far fa-envelope"></i> Email</a></p>
       </div>
     </div>
 
