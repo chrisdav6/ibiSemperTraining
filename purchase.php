d759a97a1513eba0766b871926e17d75fc814376
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-05-18 09:20:04 -0400

    Added paypal buttons to purchase page

--- purchase.php
+++ purchase.php
@@ -24,6 +24,8 @@
   <link rel="stylesheet" href="public/css/style.css">
   <!-- Favicon -->
   <link rel="icon" type="image/x-icon" href="public/img/favicon.ico">
+  <!-- Paypal Button Script -->
+  <script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD" data-sdk-integration-source="button-factory"></script>
   <title><?php echo $title; ?></title>
 </head>
 
@@ -45,8 +47,6 @@
         <h2 class="mb-4">IST T-Shirts</h2>
         <div class="info">
           <p>High quality cotton t-shirts that show IST logo on front</p>
-          <p>$20 each or $30 for 2 shirts size S through XL</p>
-          <p>$25 each for size 2XL</p>
         </div>
         <div id="tshirts" data-nanogallery2='{
           "itemsBaseURL": "./public/img/gallery/purchase/",
@@ -60,13 +60,101 @@
           <a href="tshirt1.jpg" data-ngthumb="tshirt1.jpg" data-ngdesc="Ibi Semper Training T-Shirt"></a>
           <a href="tshirt2.jpg" data-ngthumb="tshirt2.jpg" data-ngdesc="Ibi Semper Training T-Shirt"></a>
         </div>
+        <div class="pricing">
+          <p>$20 each for shirts size S through XL</p>
+          <!-- Paypal -->
+          <div class="paypal" id="paypal-button-container1"></div>
+          <script>
+          paypal.Buttons({
+            style: {
+              shape: 'rect',
+              color: 'gold',
+              layout: 'horizontal',
+              label: 'paypal',
+
+            },
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    value: '20'
+                  }
+                }]
+              });
+            },
+            onApprove: function(data, actions) {
+              return actions.order.capture().then(function(details) {
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+              });
+            }
+          }).render('#paypal-button-container1');
+          </script>
+          <!-- /Paypal -->
+          <p>$30 for 2 shirts size S through XL</p>
+          <!-- Paypal -->
+          <div class="paypal" id="paypal-button-container2"></div>
+          <script>
+          paypal.Buttons({
+            style: {
+              shape: 'rect',
+              color: 'gold',
+              layout: 'horizontal',
+              label: 'paypal',
+
+            },
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    value: '30'
+                  }
+                }]
+              });
+            },
+            onApprove: function(data, actions) {
+              return actions.order.capture().then(function(details) {
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+              });
+            }
+          }).render('#paypal-button-container2');
+          </script>
+          <!-- /Paypal -->
+          <p>$25 each for size 2XL</p>
+          <!-- Paypal -->
+          <div class="paypal" id="paypal-button-container3"></div>
+          <script>
+          paypal.Buttons({
+            style: {
+              shape: 'rect',
+              color: 'gold',
+              layout: 'horizontal',
+              label: 'paypal',
+
+            },
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    value: '25'
+                  }
+                }]
+              });
+            },
+            onApprove: function(data, actions) {
+              return actions.order.capture().then(function(details) {
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+              });
+            }
+          }).render('#paypal-button-container3');
+          </script>
+          <!-- /Paypal -->
+        </div>
       </div>
 
       <div class="purchase-group">
         <h2 class="mb-4">IST Car Window Decal - 5" x 5” </h2>
         <div class="info">
           <p>Proudly display our clear backing window decal</p>
-          <p>Only $5 each - <small class="text-danger">Temporarily out of stock, please check back soon.</small></p>
         </div>
         <div id="decals" data-nanogallery2='{
           "itemsBaseURL": "./public/img/gallery/purchase/",
@@ -78,6 +166,37 @@
           "thumbnailGutterHeight": 10
         }'>
           <a href="decal.jpg" data-ngthumb="decal.jpg" data-ngdesc="Ibi Semper Training Car Window Decal"></a>
+        </div>
+        <div class="pricing">
+          <p>Only $5 each - <small class="text-danger">Temporarily out of stock, please check back soon.</small></p>
+          <!-- Paypal -->
+          <!--<div class="paypal" id="paypal-button-container4"></div>
+          <script>
+          paypal.Buttons({
+            style: {
+              shape: 'rect',
+              color: 'gold',
+              layout: 'horizontal',
+              label: 'paypal',
+
+            },
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    value: '5'
+                  }
+                }]
+              });
+            },
+            onApprove: function(data, actions) {
+              return actions.order.capture().then(function(details) {
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+              });
+            }
+          }).render('#paypal-button-container4');
+          </script>-->
+          <!-- /Paypal -->
         </div>
       </div>
 
@@ -92,7 +211,6 @@
   <script src="public/js/smooth-scroll.min.js"></script>
   <script src="public/js/scripts.js"></script>
   <script type="text/javascript" src="https://unpkg.com/nanogallery2/dist/jquery.nanogallery2.min.js"></script>
-
 </body>
 
 </html>
