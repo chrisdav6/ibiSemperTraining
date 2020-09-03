e23e92d55562f964baaba6e581a79d273c3a6d6d
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-09-03 15:39:26 -0400

    Added paypal t-shirt button

--- purchase.php
+++ purchase.php
@@ -72,6 +72,30 @@
           <p>$20 each for shirts size S through XL</p>
           <p>$30 for 2 shirts size S through XL</p>
           <p>$25 each for size 2XL</p>
+          <div class='mt-4'>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <input type="hidden" name="cmd" value="_s-xclick">
+              <input type="hidden" name="hosted_button_id" value="MB73ZB58J5LZ4">
+              <table>
+                <tr>
+                  <td><select name="os0">
+                      <option value="1 shirt size S-XL">1 shirt size S-XL $20.00 USD</option>
+                      <option value="2 shirts size S-XL">2 shirts size S-XL $30.00 USD</option>
+                      <option value="1 shirt size 2XL">1 shirt size 2XL $25.00 USD</option>
+                    </select> </td>
+                </tr>
+                <tr>
+                  <td><input type="hidden" name="on1" value="Please enter size(s) ordered">Please enter size(s) ordered</td>
+                </tr>
+                <tr>
+                  <td><input type="text" name="os1" maxlength="200"></td>
+                </tr>
+              </table>
+              <input type="hidden" name="currency_code" value="USD">
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+              <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+            </form>
+          </div>
         </div>
       </div>
 
