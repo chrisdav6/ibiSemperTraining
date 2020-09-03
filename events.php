40f6b461f1417c39b201f9c7ddbbbaca614d6462
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-09-03 08:13:46 -0400

    Removed past event

--- events.php
+++ events.php
@@ -43,7 +43,7 @@
 
       <div class="row">
 
-        <div class="col-12 mb-5">
+        <!-- <div class="col-12 mb-5">
           <div class="longFundraiser">
             <h4><a class="text-dark fund-link" href="/public/pdf/eatForChange.pdf" target="_blank">Eat for Change!</a></h4>
             <div class="row">
@@ -65,7 +65,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
 
         <div class="col-12 mb-5">
           <div class="longFundraiser">
