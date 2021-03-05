0e8824fe35c709e34a98ab709fb53306cc1f7fe5
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-03-05 09:54:29 -0500

    Added Capital Region Chamber link to the the footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,7 @@
     <div class="row d-flex flex-column flex-lg-row">
       <div class="col text-center text-lg-left">
         <p class="address mb-0 text-secondary">&copy; <?php echo date("Y"); ?> Ibi Semper Training (IST)</p>
+        <p class="chamber mt-2"><a href="https://capitalregionchamber.com" target="_black">Proud Member - <img src="/public/img/capitalRegionChamber.svg" alt="Capital Region Chamber" /></a></p>
       </div>
       <div class="col text-center text-lg-right">
         <p class="address mb-0 text-secondary"><a href="tel:15186053976" class="text-secondary mr-2"><i class="fas fa-phone-alt"></i> Phone: 518-605-3976</a> / <a href="mailto:ibisempertraining@gmail.com?subject=Ibi Semper Website Inquiry" class="text-secondary ml-2"><i class="far fa-envelope"></i> Email</a></p>
