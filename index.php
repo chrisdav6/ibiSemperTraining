086bd277d6a17711a41dfae3ac8300f19c32b86f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-04-29 08:00:04 -0400

    Added Facebook Chat script

--- index.php
+++ index.php
@@ -90,6 +90,31 @@
   <script src="public/js/scripts.js"></script>
   <script src="public/js/facebookPixel.js"></script>
 
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml: true,
+      version: 'v10.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  </script>
+
+  <!-- Your Chat Plugin code -->
+  <div class="fb-customerchat" attribution="install_email" page_id="101250784762442">
+  </div>
+
+
 </body>
 
 </html>
