3af306d2f04f48166e27fd16da512e84dab0f6e1
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-07-02 10:38:21 -0400

    Added new events

--- events.php
+++ events.php
@@ -54,7 +54,7 @@
                 </a>
               </div>
               <div class="col-lg-9 col-md-8">
-                <h5>Dec 14th from 4:30pm - 7:30pm - <a class="link text-dark" href="https://goo.gl/maps/BEut4AnvMPjge2mj8" target="_blank">Mohwak Commons 400 Balltown Rd</a></h5>
+                <h5>December 14th from 4:30pm - 7:30pm - <a class="link text-dark" href="https://goo.gl/maps/BEut4AnvMPjge2mj8" target="_blank">Mohwak Commons 400 Balltown Rd</a></h5>
                 <p>Come and eat some delicious pizza at Blaze Pizza while supporting IBI. Blaze Pizza will donate 20% of proceeds from your meal back to our organization. </p>
                 <p class='mb-1'>
                   <strong>Two ways to support</strong>
@@ -78,13 +78,13 @@
                 </a>
               </div>
               <div class="col-lg-9 col-md-8">
-                <h5>Mar 31st from 5pm - 8pm - <a class="link text-dark" href="https://goo.gl/maps/BEut4AnvMPjge2mj8" target="_blank">Mohwak Commons 400 Balltown Rd</a></h5>
+                <h5>March 31st from 5pm - 8pm - <a class="link text-dark" href="https://goo.gl/maps/BEut4AnvMPjge2mj8" target="_blank">Mohwak Commons 400 Balltown Rd</a></h5>
                 <p>Come and eat some delicious pizza at Blaze Pizza while supporting IBI. Blaze Pizza will donate 20% of proceeds from your meal back to our organization. </p>
                 <p>Use <a class='link text-dark' href='public/pdf/blazePizza12142020.pdf' target='_blank'>this flyer</a> at checkout to support Veterans & First Responders to train their dogs as PTSD Service Dogs, at no charge to the teams. Our fundraisers pays for their training and dog adoption fees. Thank you for your support!</p>
               </div>
             </div>
           </div>
-        </div> -->
+        </div>
 
         <div class="col-12 mb-5">
           <div class="longFundraiser">
@@ -101,7 +101,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
 
         <div class="col-12 mb-5">
           <div class="longFundraiser">
@@ -113,10 +113,28 @@
                 </a>
               </div>
               <div class="col-lg-9 col-md-8">
-                <h5>Aug 28th at 10:00am - <a class="link text-dark" href="https://g.page/BrunswickHarley?share" target="_blank">Brunswick Harley-Davidson</a></h5>
+                <h5>August 28th at 10:00am - <a class="link text-dark" href="https://g.page/BrunswickHarley?share" target="_blank">Brunswick Harley-Davidson</a></h5>
                 <p>Please join us for our first PTSD & Service Dog Awareness Ride! Enjoy a great ride, ceremony, food, vendors and raffle items. All proceeds directly benefit Veterans and First Responders with PTSD as well as Rescue/Shelter dogs.
                 </p>
                 <p>Our Training Program is free to Veterans and First Responders with PTSD! We are funded solely though Fundraisers & Donations!! Only $20 per bike & rider. $10 for passengers. Dogs are free!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 mb-5">
+          <div class="longFundraiser">
+            <h4 class='link'><a class="text-dark fund-link" href="https://www.facebook.com/tomsburnthills" target="_blank">2nd Annual Cuts4Pups</a></h4>
+            <div class="row">
+              <div class="col-lg-3 col-md-4 mb-4">
+                <a href="https://www.facebook.com/tomsburnthills" target="_blank">
+                  <img class="img-fluid" src="/public/img/cutsPups.png" alt="Cuts Benefiting Pups">
+                </a>
+              </div>
+              <div class="col-lg-9 col-md-8">
+                <h5>October 3rd - <a class="link text-dark" href="https://goo.gl/maps/JD1L93HtDMLojfH88" target="_blank">795 Saratoga Rd, Burnt Hills, NY</a></h5>
+                <p>Sons, Brothers, Fathers, Uncles, Dads and Grandads: Get a haircut and benefit PTSD Service Dogs and their Veteran and 1st Responder handlers. We operate solely on donations and Fundraisers! Your support is much appreciated!
+                </p>
               </div>
             </div>
           </div>
@@ -128,7 +146,7 @@
 
       <div class="row">
 
-        <div class="col-12 mb-5">
+        <!-- <div class="col-12 mb-5">
           <div class="longFundraiser">
             <h4><a class="text-dark fund-link" href="https://www.facebook.com/crazytrain.ridersclub" target="_blank">8th Annual Suicide Prevention Run</a></h4>
             <div class="row">
@@ -140,6 +158,42 @@
               <div class="col-lg-9 col-md-8">
                 <h5>May 1st at 11:00am - <a class="link text-dark" href="https://goo.gl/maps/4LhSaEVLgKCkpMvd8" target="_blank">1263 Patten Mills Road, Fort Ann</a></h5>
                 <p>Please join this Suicide Prevention Motorcycle Run at Boar’s Nest Bar & Grill and say hi to the IST Vendor team. Support this worthy cause to prevent suicide in our Community.</p>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="col-12 mb-5">
+          <div class="longFundraiser">
+            <h4><a class="text-dark fund-link" href="https://albany-saratogaspeedway.com" target="_blank">Military Appreciation Night at the Albany-Saratoga Speedway</a></h4>
+            <div class="row">
+              <div class="col-lg-3 col-md-4 mb-4">
+                <a href="https://albany-saratogaspeedway.com" target="_blank">
+                  <img class="img-fluid" src="/public/img/racingNight2021.png" alt="Military Appreciation Night">
+                </a>
+              </div>
+              <div class="col-lg-9 col-md-8">
+                <h5>July 2nd at 5:00pm - <a class="link text-dark" href="https://goo.gl/maps/oNJYShKtDoiwy4aC7" target="_blank">2671 U.S. 9, Ballston Spa, NY 12020</a></h5>
+                <p>Please join us tonight at the Albany-Saratoga Speedway for a fun night. IST will be a Vendor at the Military Awareness Night and you can find out more about our program and meet us, as well as some of our teams. We look forward to seeing you there.
+                </p>
+                <a href="/public/pdf/racingMilitaryNight2021.pdf" target="_blank"' class="btn btn-dark">Download Flyer</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 mb-5">
+          <div class="longFundraiser">
+            <h4><a class="text-dark fund-link" href="https://www.visitlakegeorge.com/events/pet-fest" target="_blank">Pet Fest</a></h4>
+            <div class="row">
+              <div class="col-lg-3 col-md-4 mb-4">
+                <a href="https://www.visitlakegeorge.com/events/pet-fest" target="_blank">
+                  <img class="img-fluid" src="/public/img/petFest.png" alt="Pet Fest 2021">
+                </a>
+              </div>
+              <div class="col-lg-9 col-md-8">
+                <h5>September 18th at 11:00am - <a class="link text-dark" href="https://goo.gl/maps/nAhak4UcgY5tS7JbA" target="_blank">Maple Street, Glens Falls, NY 12801</a></h5>
+                <p>We are a vendor at PetFest! Stop by and say Hi! 👋 Pet Fest, held in Glens Falls City Park, brings pets and their owners together to showcase the pets and animal sector of the Glens Falls Collaborative. This event was created to show just how pet-friendly Glens Falls can be!</p>
               </div>
             </div>
           </div>
