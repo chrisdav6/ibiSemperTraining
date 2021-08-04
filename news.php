103dc54e88c2a741e3d169c9f48e5217b6e149ce
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-08-04 16:04:08 -0400

    Added news item

--- news.php
+++ news.php
@@ -45,6 +45,11 @@
         <p>Highlighting the importance of service dogs. (read more)</p>
       </a>
 
+      <a class="newsItem" href="https://www.timesunion.com/news/article/Your-Best-Shot-Service-Dogs-16343398.php" target="_blank">
+        <span>July 27, 2021</span>Your Best Shot: Service Dogs
+        <p>Ibi Semper Training (IST), Latin for “Always There,” is helping veterans and first responders while rescuing dogs, and pairing them together to train them to become Post Traumatic Stress Disorder (PTSD) Service Dog teams. (read more)</p>
+      </a>
+
       <a class="newsItem" href="https://www.timesunion.com/news/article/Art-program-aims-to-help-psychological-effects-of-16084975.php" target="_blank">
         <span>April 7, 2021</span>Ibi Semper Training IN THE NEWS
         <p>Arts4Vets is such a wonderful program and we thank them and the American Legion Post 1520 for their support for and partnering with Ibi Semper Training, Inc! Please read and reach out, it can only benefit you. (read more)</p>
