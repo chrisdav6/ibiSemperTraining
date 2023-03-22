5b8d3137e0053f098e7a34333a142c0105f25e7e
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-03-22 07:32:45 -0400

    Added Knitt link to footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,9 @@
       <div class="col text-center text-lg-left">
         <p class="address mb-0 text-secondary">&copy; <?php echo date("Y"); ?> Ibi Semper Training (IST)</p>
         <p class="chamber mt-2"><a href="https://capitalregionchamber.com" target="_black">Proud Member <img class='px-1' src="/public/img/capitalRegionChamber.svg" alt="Capital Region Chamber" /> Since 2021</a></p>
+        <p>
+          <a href="https://app.getknitt.com/" target="_blank" data-wpel-link="external" rel="external noopener noreferrer"><img width="120px" src="https://getknitt.com/wp-content/uploads/2023/02/knitt_web-badge_color-small.svg" alt="visit getknitt.com badge" /></a>
+        </p>
       </div>
       <div class="col text-center text-lg-right">
         <p class="address mb-0 text-secondary"><a href="tel:15186053976" class="text-secondary mr-2"><i class="fas fa-phone-alt"></i> Phone: 518-605-3976</a> / <a href="mailto:ibisempertraining@gmail.com?subject=Ibi Semper Website Inquiry" class="text-secondary ml-2"><i class="far fa-envelope"></i> Email</a></p>
