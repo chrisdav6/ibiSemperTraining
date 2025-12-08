6f5a392672857ab3225de75a99a7b43c1a98d7ac
Chris Davis <chris.davis5440@gmail.com>
2025-12-08 07:43:37 -0500

    Added PTS 2026 Ride

--- events.php
+++ events.php
@@ -113,6 +113,26 @@
           </div>
         </div>
 
+        <div class="col-12 mb-5">
+          <div class="longFundraiser">
+            <h4><a class="text-dark fund-link" href="public/pdf/PTSDRIDE2026.pdf" target="_blank">PTS AWARENESS RIDE</a></h4>
+            <div class="row">
+              <div class="col-lg-3 col-md-4 mb-4">
+                <a href="public/pdf/PTSDRIDE2026.pdf" target="_blank">
+                  <img class="img-fluid" src="/public/img/PTSDRIDE2026.png" alt="PTS & Service Dog Awareness Ride 2026">
+                </a>
+              </div>
+              <div class="col-lg-9 col-md-8">
+                <h5>August 1st (Rain Date 8th) - 10am - KSU at 12pm - <a class="link text-dark" href="https://g.page/BrunswickHarley?share" target="_blank">Brunswick Harley-Davidson</a></h5>
+                <p>Please join us for our 6th annual PTS & Service Dog Awareness motorcycle ride at Brunswick Harley Davidson. You will be able to enjoy vendors, entertainment and we will have lunch and raffles at our end destination. You will support a Veterans & First Responders program that improves the lives of humans, as well as shelter dogs. Our Vendors are either Veteran owned businesses, benefitting Veterans or dogs. Ride and or stop by backing a worthy cause that supports your local community. Thank you.</p>
+                <!-- <a class='btn btn-primary btn-sm' href="https://forms.gle/HyigqiEvE323E71g9" target="_blank">
+                  Register Here
+                </a> -->
+              </div>
+            </div>
+          </div>
+        </div>
+
         <!-- 
         <div class="col-12 mb-5">
           <div class="longFundraiser">
@@ -128,26 +148,6 @@
                 <p>Get pumped and start the day with a friendly group of people! Bring your family, friends, co-workers and other like-minded people for a great way to get
                   ready for your day. Two sessions - at 7:30am and at 9am on June 21st. Be there or be square. Thank you for your continued support!
                 </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-12 mb-5">
-          <div class="longFundraiser">
-            <h4><a class="text-dark fund-link" href="public/pdf/PTSDRIDE2025.pdf" target="_blank">PTS AWARENESS RIDE</a></h4>
-            <div class="row">
-              <div class="col-lg-3 col-md-4 mb-4">
-                <a href="public/pdf/PTSDRIDE2025.pdf" target="_blank">
-                  <img class="img-fluid" src="/public/img/PTSDRIDE2025.png" alt="PTS & Service Dog Awareness Ride 2025">
-                </a>
-              </div>
-              <div class="col-lg-9 col-md-8">
-                <h5>August 2nd (Rain Date 9th) - 10am - KSU at 12pm - <a class="link text-dark" href="https://g.page/BrunswickHarley?share" target="_blank">Brunswick Harley-Davidson</a></h5>
-                <p>Please join us for our 5th annual PTS & Service Dog Awareness motorcycle ride at Brunswick Harley Davidson. You will be able to enjoy vendors, entertainment and we will have lunch and raffles at our end destination. You will support a Veterans & First Responders program that improves the lives of humans, as well as shelter dogs. Our Vendors are either Veteran owned businesses, benefitting Veterans or dogs. Ride and or stop by backing a worthy cause that supports your local community. Thank you.</p>
-                <a class='btn btn-primary btn-sm' href="https://forms.gle/HyigqiEvE323E71g9" target="_blank">
-                  Register Here
-                </a>
               </div>
             </div>
           </div>
