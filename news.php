2b556706b5f01b5cf0b2249652400fbde2e20138
Chris Davis <chris.davis5440@gmail.com>
2026-02-13 15:30:14 -0500

    Added news article

--- news.php
+++ news.php
@@ -230,6 +230,11 @@
   <div id="news" class="secondary-content mt-4 mb-5">
     <div class="container">
 
+      <a class="newsItem" href="https://www.cnn.com/2026/02/12/health/dog-food-toxins-wellness?utm_source=cnn_Evening+Newsletter+-+Thursday%2C+February+12%2C+2026&utm_medium=email&bt_ee=IFHRE30g4tfnmeVUngJuE3kuHxD5itLAN%2Fk3a04CcCsS9YRnda8d71BuE5tqsueE&bt_ts=1770936147723" target="_blank">
+        <span>February 2026</span>Commercial dog food contains ‘alarming’ levels of lead, mercury and other contaminants
+        <p>Samples of popular commercial dog foods contained “dangerous levels” of heavy metals, plastic contaminants, and acrylamide, a cancer-causing compound created when food is subjected to high heat, according to a new investigation.... (read more)</p>
+      </a>
+
       <a class="newsItem" href="https://www.news-medical.net/news/20251007/Working-with-service-dogs-may-help-slow-biological-aging-in-female-veterans.aspx?fbclid=IwY2xjawNXs0xleHRuA2FlbQIxMABicmlkETFGUUd6QWlOMXhETHBNU2xHAR7dGap4lBpBS18EN6kXRgIN_TfkzcU-llxEL4W0_WF7fhWNL3HY_Szfd09fBQ_aem_mDO6OuEOi2CsfxTIqN7_Gg" target="_blank">
         <span>October 2025</span>Working with service dogs may help slow biological aging in female veterans
         <p>New research finds that "man's best friend" may help slow biological aging in women. This groundbreaking study, focused on female veterans in the United States, is among the first to examine the impact of working with service dogs on this often-overlooked population... (read more)</p>
